e9928ad651c717542eeb51ada52517a00d007226
JuliaCarew <crewjulia3@gmail.com>
2024-11-06 16:25:50 -0400

    got string written to game screen, need to make it write in a square, got rid of player physics(need to make it grid based)

--- Assets/Scripts/MapBehaviors.cs
+++ Assets/Scripts/MapBehaviors.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.IO;
+using TMPro;
+using static Unity.Burst.Intrinsics.X86.Avx;
 
 public class MapBehaviors : MonoBehaviour
 {
@@ -16,17 +19,21 @@ public class MapBehaviors : MonoBehaviour
 
     // Tile char variables
     public char wall = '#';
-    public char door = '0';
+    public char door = 'O';
     public char chest = '*';
     public char enemy = '@';
+    static char empty = ' ';
 
     // collision variables
     bool canWalk;
     bool outOfBounts;
 
+    public TextMeshProUGUI tmp;
+
     void Start()
     {
-        
+        LoadPremadeMap();
+        GenerateMapString();
     }
 
     void Update()
@@ -35,32 +42,83 @@ public class MapBehaviors : MonoBehaviour
     }
 
     // Returns a string representation of a randomly generated map
-    int GenerateMapString(int width, int height)
-    { // set up multidimensional array for tilemap to be generated
-        for (int x = 0; x < myMap.GetLength(0); x++)
+    string GenerateMapString()
+    {
+        tmp.text = "";
+        // set up multidimensional array for tilemap to be generated
+        for (int x = 0; x < 20; x++)
         {
-            for (int y = 0; y < myMap.GetLength(1); y++)
-            {
-                //TileRules();
-                myMap[x, y] = Random.Range(0, 4);
+            for (int y = 0; y < 20; y++)
+            {   
+                // applying rules before generating the map
+                TileRules();
 
-                //if (myMap[x, y] == 0) // set char wall
-                //if (myMap[x, y] == 1) // set char door
-                //if (myMap[x, y] == 2) // set char chest
-                //if (myMap[x, y] == 3) // set char enemy
+                myMap[x, y] = Random.Range(0, 4);
+                var mapStringResult = myMap[x, y];
+
+                if (myMap[x, y] == 0)
+                {
+                    // set char wall
+                    tmp.text += wall;
+                    Debug.Log("set char wall");
+                }
+                if (x >=5 && y >= 5 && x <= 15 && y <= 15)
+                {
+                    // set char empty
+                    tmp.text += empty;
+                    Debug.Log("set char empty");
+                }
+                if (myMap[x, y] == 1)
+                {
+                    // set char door
+                    tmp.text += door;
+                    Debug.Log("set char door");
+                }
+                if (myMap[x, y] == 2)
+                {
+                    // set char chest
+                    tmp.text += chest;
+                    Debug.Log("set char chest");
+                }
+                if (myMap[x, y] == 3)
+                {
+                    // set char enemy
+                    tmp.text += enemy;
+                    Debug.Log("set char enemy");
+                }               
             }
         }
-        return(width,height);
+        return($"mapStringResult");
+        // need to adjust range so writing to the walls and not everywhere in between
     }
-    // ?? don't need nested for loop because i will get width/height as a result of reading the map ??
-    // ?? don't need to convert the chars to their tiles until next method ??
-    // !! this method generates a random map based on the characters given, using the tile rules !!
-    public void CheckNeighbor()
+    public int CheckNeighbors()
     {
         // check the surrounding 8 tiles to be used in TileRules()
+        int countWall = 0;
+        int countDoor = 0;
+        int countChest = 0;
+        int countEnemy = 0;
+
+        for (int check_x = -1; check_x < 2; check_x++)
+        {
+            for (int check_y = -1; check_y < 2; check_y++)
+            {   // don't count current tile
+                if (check_x == 0 && check_y == 0)
+                {
+                    var tile = new Vector3Int(check_x, check_y);
+                    continue;
+                }
+                // else if ( neighbor char == wall ) -- countWall++;
+            }
+        }
+        return countWall;
+        return countDoor;
+        return countChest;
+        return countWall;
     }
-    public void TileRules()
+    void TileRules()
     {
+        CheckNeighbors();
         // Doors always need to be within 1 tile of walls 
         // Chests must be at least 2 tiles away from doors, and within 1 tile of a wall 
         // Enemies need to be 2 tiles away from nearest occupied tile of any kind 
@@ -73,12 +131,18 @@ public class MapBehaviors : MonoBehaviour
     // Converts a map string into Unity Tilemap
     string ConvertMapToTilemap(string mapData)
     {
+        //if (char wall){
+        //  Tile _wall = myMap.GetTile(#);
+        //}
         return (" ");
     }
 
     // Loads a pre-made map from a text asset
-    string LoadPremadeMap(string mapFilePath)
+    string LoadPremadeMap()
     {
-        return (" ");
+        Debug.Log("reading text file");
+        string maptxt = File.ReadAllText("C:/Users/W0517383/Documents/2DMapStrings/TestMap.txt");
+        //Read the text from directly from the test.txt file            
+        return (maptxt);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed = 2.0f;
-    private Rigidbody2D rb;
-    private Vector2 movementDirection;
+    //[SerializeField] private float movementSpeed = 2.0f;
+    //private Rigidbody2D rb;
+    //private Vector2 movementDirection;
 
-    void Start()
-    { // getting the rigidbody component of the current gameobject
-        rb = GetComponent<Rigidbody2D>();
-    }
-    // movement functions
-    void Update()
-    { // specify what direction we will move in depending on button pressed
-        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-    }
-    // physics functions
-    void FixedUpdate()
-    {
-        rb.velocity = movementDirection * movementSpeed;
-    }
+    //void Start()
+    //{ // getting the rigidbody component of the current gameobject
+    //    rb = GetComponent<Rigidbody2D>();
+    //}
+    //// movement functions
+    //void Update()
+    //{ // specify what direction we will move in depending on button pressed
+    //    movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    //}
+    //// physics functions
+    //void FixedUpdate()
+    //{
+    //    rb.velocity = movementDirection * movementSpeed;
+    //}
 }
+// needs to not be physics (check each tile & move player along in array?
+// create empty layer for player movement grid to draw/erase for button pressed
